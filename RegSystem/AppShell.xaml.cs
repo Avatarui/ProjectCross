4776efff324e834fca38f21a6bd02ae62f09c615
Avatarui1 <65011212186@msu.ac.th>
2025-03-27 22:08:52 +0700

    first commit

---
+++ RegSystem/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace RegSystem;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ RegSystem/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace RegSystem;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
